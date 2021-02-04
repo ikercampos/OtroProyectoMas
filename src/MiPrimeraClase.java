289753062bf08045cb92f6c39ba1565e8d37e010
Iker Campos <icampos009@ikasle.ehu.eus>
2021-02-04 16:53:31 +0100

    Versión inicial

---
+++ src/MiPrimeraClase.java
@@ -0,0 +1,14 @@
+
+public class MiPrimeraClase {
+
+	public static void main(String[] args) {
+		System.out.println("Esto es un mensaje");
+		otroMetodo();
+
+	}
+	
+	private static void otroMetodo() {
+		System.out.println("Otro metodo");
+	}
+	
+}
